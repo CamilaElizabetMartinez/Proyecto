067292ade77591dbfc17fe425efdf87be51f6a9e
camila martinez <martinezce_@hotmail.com>
2021-07-31 13:23:38 +0300

    Encapsulamos Router con AthContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
-import Footer from "./components/Footer/Footer";
 import Router from './routers/Router';
+import AuthContext from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="App">     
-      <Router />
-      <Footer />
+    <div className="App">
+      <AuthContext>
+        <Router />
+      </AuthContext>
     </div>
   );
 };
 
 export default App;
+
+/* import './App.css';
+import Router from './router/Router';
+import AuthContext from "./context/AuthContext";
+
+export default function App() {
+  return (
+    <div>
+      <AuthContext>
+        <Router />
+      </AuthContext>
+    </div>
+  )
+} */
