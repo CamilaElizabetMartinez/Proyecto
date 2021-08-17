208b829241553d6245a454e80909f1093d96d091
camila martinez <martinezce_@hotmail.com>
2021-08-17 11:54:09 +0300

    feat(router): añadí componete AuthContext

--- client/src/routers/Router.js
+++ client/src/routers/Router.js
@@ -1,4 +1,5 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
@@ -9,24 +10,30 @@ import joinAsAproducer from "../pages/joinAsAproducer";
 import aboutUs from "../pages/aboutUs";
 import login from "../pages/login";
 import passwordReset from "../pages/passwordReset";
+import createProduct from "../pages/createProduct";
+import productIndex from "../pages/admin/product/productIndex";
 
 
 export default function Router() {
     return (
         <div>
-            <BrowserRouter>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={home}/>
-                    <Route path="/noticias" component={news}/>
-                    <Route path="/haz-tu-pedido" component={makeYourOrder}/>
-                    <Route path="/unete-como-productor" component={joinAsAproducer}/>
-                    <Route path="/quienes-somos" component={aboutUs}/>
-                    <Route path="/iniciar-sesion" component={login}/>
-                    <Route path="/haz-olvidado-tu-contraseña" component={passwordReset}/>
-                </Switch>
-            </BrowserRouter>
-            <Footer/>
+            <AuthContext>
+                <BrowserRouter>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={home} />
+                        <Route path="/noticias" component={news} />
+                        <Route path="/haz-tu-pedido" component={makeYourOrder} />
+                        <Route path="/unete-como-productor" component={joinAsAproducer} />
+                        <Route path="/quienes-somos" component={aboutUs} />
+                        <Route path="/iniciar-sesion" component={login} />
+                        <Route path="/haz-olvidado-tu-contraseña" component={passwordReset} />
+                        <Route path="/producto/crear" component={createProduct} />
+                        <Route path="/producto/index" component={productIndex} />
+                    </Switch>
+                </BrowserRouter>
+            </AuthContext>
+            <Footer />
         </div>
     );
 };
