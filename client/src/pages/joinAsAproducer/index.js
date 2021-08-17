bd0b932c32d566b8fe42209078912e7649ea785f
camila martinez <martinezce_@hotmail.com>
2021-08-17 12:07:26 +0300

    feat(form):añadimon link

--- client/src/pages/joinAsAproducer/index.js
+++ client/src/pages/joinAsAproducer/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./joinAsAproducer.css";
 import { useForm } from "../../hooks/userForm";
+import {Link} from "react-router-dom";
 
 
 function JoinAsAproducer() {
 
-    const formInitialState = {name: "", surname: "", city:"", phone: "", email:""};
+    const formInitialState = { name: "", surname: "", city: "", phone: "", email: "" };
     const [form, handleChange] = useForm(formInitialState);
 
     const handleSubmit = async e => {
         e.preventDefault();
-    }; 
+    };
 
     return (
         <div className="container">
@@ -25,17 +26,21 @@ function JoinAsAproducer() {
                         </div>
 
                         <div className="inputBlock">
-                            <input onChange={handleChange} className="form" value={form.name}    name="name"     type="text"   placeholder="Nombre" /> 
-                            <input onChange={handleChange} className="form" value={form.surname} name="surname"  type="text"   placeholder="Apellido" />
-                            <input onChange={handleChange} className="form" value={form.country} name="city"     type="text"   placeholder="Ciudad" />
-                            <input onChange={handleChange} className="form" value={form.address} name="address"  type="text"   placeholder="Dirección" />
-                            <input onChange={handleChange} className="form" value={form.phone}   name="phone"    type="text"   placeholder="Teléfono" />
-                            <input onChange={handleChange} className="form" value={form.email}   name="email"    type="email"  placeholder="Email" />
-                
+                            <input onChange={handleChange} className="form" value={form.name} name="name" type="text" placeholder="Nombre" />
+                            <input onChange={handleChange} className="form" value={form.surname} name="surname" type="text" placeholder="Apellido" />
+                            <input onChange={handleChange} className="form" value={form.country} name="city" type="text" placeholder="Ciudad" />
+                            <input onChange={handleChange} className="form" value={form.address} name="address" type="text" placeholder="Dirección" />
+                            <input onChange={handleChange} className="form" value={form.phone} name="phone" type="text" placeholder="Teléfono" />
+                            <input onChange={handleChange} className="form" value={form.email} name="email" type="email" placeholder="Email" />
+
                             <div className="inputTermConditions">
                                 <input type="checkbox" id="termConditions" name="termConditions" value="conditions" />
                                 <label for="termConditions"><a href="https://www.youtube.com/watch?v=FDMq9ie0ih0">He leído y acepto la Política de Privacidad.</a></label>
-                            </div> 
+                            </div>
+
+                            <div className="inputTermConditions">
+                                <Link to="/iniciar-sesion">¿Ya tienes una cuenta con nosotros?</Link>
+                            </div>
 
                             <div className="inputSubmit">
                                 <input type="submit" value="Enviar" className="btnSubmit" />
@@ -45,7 +50,7 @@ function JoinAsAproducer() {
                     </form>
                 </div>
             </div>
-        </div>      
+        </div>
     );
 };
 export default JoinAsAproducer;
