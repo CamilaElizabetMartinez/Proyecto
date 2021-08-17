ef9a351a7b2bc3febeb7a771ab25a36991d434d1
camila martinez <martinezce_@hotmail.com>
2021-08-17 11:52:27 +0300

    feat(ListsProducts,form): añadí tabla y formulario para admin

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export default function NewProductForm({ setProducts }) {
+
+    const initialProductsState = {
+        name: "",
+        category_id: "",
+        weight: "",
+        price: "",
+        imageProduct: "",
+        img_principal: "",
+
+    }
+
+    const [form, setForm] = useState(initialProductsState);
+    function handleInput(e) {
+        const inputName = e.target.id;
+        const newValue = e.target.value;
+
+        setForm({...form, ...{[inputName]: newValue}})
+    } 
+
+    function submit(e) {
+        e.preventDefault();
+
+        const newProduct = {
+            name: form.name,
+            category_id: form.category_id,
+            weight: form.weight,
+            price: form.price,
+            imageProduct: form.imageProduct,
+            img_principal: form.img_principal,
+
+        };
+        //setProducts([...contacts, newProduct]) //Necesitaria recibir "contacts"
+        setProducts(currentProducts => [...currentProducts, newProduct]);
+        //e.target.reset(); 
+        setForm(initialProductsState);//reiniciamos el formulario
+    }
+
+    return (
+        <form className="form" onSubmit={submit}>
+
+            <input className="form-control mb-3" type="text" id="name" value={form.name} onChange={handleInput} placeholder="Producto" />
+            <input className="form-control mb-3" type="text" id="category_id" value={form.category_id} onChange={handleInput} placeholder="Categoria" />
+            <input className="form-control mb-3" type="text" id="weight" value={form.weight} onChange={handleInput} placeholder="Peso" />
+            <input className="form-control mb-3" type="text" id="price" value={form.price} onChange={handleInput} placeholder="Precio" />
+            <label>Imagen del producto</label>
+            <input className="form-control mb-3" type="file" id="imageProduct" accept="png jpg jpeg" value={form.imageProduct} onChange={handleInput} />
+            <label>Imagen principal</label>
+            <input className="form-control mb-3" type="file" id="img_principal" accept="png jpg jpeg" value={form.img_principal} onChange={handleInput} />
+            <input type="submit" className="btnSubmit" value="Enviar" />
+        </form>
+    )
+}
---
+++ client/src/components/ListsProducts/ListsProducts.js
@@ -0,0 +1,56 @@
+import './listsproducts.css';
+import ButtonCreateProduct from "../ButtonCreateProduct/ButtonCreateProduct";
+import { useHistory } from "react-router-dom";
+
+function ListsProducts({ products }) {
+
+    const history = useHistory();
+
+    function handleAdd() {
+        history.push("/producto/crear");
+    }
+
+    return (
+        <div className="container">
+            <div className="containerBlockPanelAdmin buttonCreateproduct">
+                <div className="block">
+                    <ButtonCreateProduct onClick={handleAdd} />
+                </div>
+            </div>
+            <div className="">
+                <h1 className="mt-4">Productos</h1>
+
+                <table className="table">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Categoria</th>
+                            <th scope="col">Peso</th>
+                            <th scope="col" id="priceHeader">Precio</th>
+                            <th scope="col">Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tablebody">
+                        {products.map((products, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{products.name}</td>
+                                    <td>{products.category_id}</td>
+                                    <td>{products.weight}</td>
+                                    <td>{products.price}</td>
+                                </tr>
+                            )
+                        })
+                        }
+                    </tbody>
+                    <tfoot></tfoot>
+                </table>
+            </div>
+        </div>
+
+
+    )
+};
+
+
+export default ListsProducts;
