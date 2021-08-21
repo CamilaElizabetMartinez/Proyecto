bc063fd00fa1e73bbf687f21111f650743b684d0
camila martinez <martinezce_@hotmail.com>
2021-08-21 16:46:43 +0300

    refactor(app) eliminado authcontext

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import Router from './routers/Router';
-import AuthContext from "./context/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      <AuthContext>
-        <Router />
-      </AuthContext>
+      <Router />
     </div>
   );
 };
