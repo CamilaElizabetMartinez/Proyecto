3228fe424ab9e34e2ee2afa07633f96f497e645c
camila martinez <martinezce_@hotmail.com>
2021-08-27 18:09:01 +0300

    feat(productIndex): creamos index para los usuarios admin

---
+++ client/src/pages/admin/product/productIndex.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+import { useAuthContext } from "../../../context/AuthContext";
+import { PRODUCT_INDEX_URL } from "../../../settings";
+import ListsProducts from '../../../components/ListsProducts/ListsProducts';
+
+
+function ProductIndex() {
+
+    const [products, setProducts] = useState([]);
+
+    const { getAuthHeaders, isAuthenticated } = useAuthContext();
+
+    /* const {getAuthHeaders, isAuthenticated } = useAuthContext();
+    const [products, setProducts] = useState("");
+ */
+    useEffect(() => {
+
+        const options = {
+            headers: getAuthHeaders({
+                "Content-type": "application/json",
+                "Accept": 'application/json'
+            })
+        };
+
+        fetch(PRODUCT_INDEX_URL, options)
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    /*   useEffect(() => {
+          const options = {headers: getAuthHeaders()};
+  
+          fetch(PRODUCT_INDEX_URL, options)
+          .then(response => {
+              if(!response.ok) throw new Error(response.statusText)
+              return response.json();
+          })
+          .then(data => setProducts(data.message))
+          .catch(err => {
+              console.log(err, "You are not authorized to see this.");
+              return <Redirect to="/inisiar-sesion" />;
+          });
+      }, [ isAuthenticated, getAuthHeaders]) */
+
+    return (
+        <div>
+            <ListsProducts products={products} />
+        </div>
+    )
+}
+export default ProductIndex;
