bfac7264bad93ff38d182ff31af641dd47893a86
camila martinez <martinezce_@hotmail.com>
2021-08-27 18:05:38 +0300

    feat(Settings): creamos constates

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,9 +1,14 @@
 const API_BASE_URL = "http://comemoscomopensamos.local/";
 
-const LOGIN_URL = API_BASE_URL+'api/login_check'
+const LOGIN_URL = API_BASE_URL + 'api/login_check';
+
+const VALIDATE_SESSION = API_BASE_URL + 'api/session';
+
+const PRODUCT_INDEX_URL = API_BASE_URL + 'api/product';
 
 export {
     API_BASE_URL,
     LOGIN_URL,
-    
+    PRODUCT_INDEX_URL,
+    VALIDATE_SESSION,
 };
