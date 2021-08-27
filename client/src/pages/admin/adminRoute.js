0c2811515c0e5190f3cc04792b6dc7c16c6fdb83
camila martinez <martinezce_@hotmail.com>
2021-08-27 18:12:27 +0300

    feat(adminRoute): creamos logica para la seguridad de las paginas

---
+++ client/src/pages/admin/adminRoute.js
@@ -0,0 +1,16 @@
+import { Redirect, Route } from "react-router";
+import { useAuthContext } from "../../context/AuthContext";
+
+
+export default function AdminRoute({ children, ...rest }) {
+
+    const { isAuthenticated, isAdmin } = useAuthContext();
+
+    return (
+        <Route {...rest} render={() => {
+            return isAuthenticated && isAdmin()
+                ? children
+                : <Redirect exact to="/iniciar-sesion" />
+        }} />
+    )
+} 
