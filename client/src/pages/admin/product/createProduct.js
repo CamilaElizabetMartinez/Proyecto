7dc650ebf33e4b766669920f6824a912a900e4ea
camila martinez <martinezce_@hotmail.com>
2021-08-27 18:11:10 +0300

    feat(createProduct): creamos createProduct

---
+++ client/src/pages/admin/product/createProduct.js
@@ -0,0 +1,10 @@
+import Form from '../../../components/Form/Form';
+
+function CreateProduct() {
+    return ( 
+        <div>
+            <Form />
+        </div>
+    )
+}
+export default CreateProduct;
