395dad4fcba881f8319d6b3115f9af0413f7cc93
camila martinez <martinezce_@hotmail.com>
2022-02-25 17:16:01 +0200

    refactpr(client/src): Creacion de import y estado para category

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
-import './App.css';
-import Router from './routers/Router';
+import React from 'react'
+import { useState, useEffect} from 'react'
+import { API_CATEGORY } from './settings'
+import {AppProvider} from './context/AppContext'
+import Router from './routers/Router'
+import './App.css'
+
+  const [categories, setCategories] = useState(null);
 
 function App() {
   return (
