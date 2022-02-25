9e7331b904968f193043d379df3c63de74200ed0
camila martinez <martinezce_@hotmail.com>
2022-02-25 18:26:00 +0200

    refactor(client/src/pages/news): Creacion de useEffect y fetch al endpoint

--- client/src/pages/news/index.js
+++ client/src/pages/news/index.js
@@ -6,6 +6,13 @@ import { INDEX_NEWS_PUBLIC } from '../../settings'
 const News = () => {
     //Se declara el estado de noticias
     const [ news, setNews ] = useState([]);
+    //Se realiza la consulta a la api 
+    useEffect(() => {
+        fetch(INDEX_NEWS_PUBLIC)
+            .then(response => response.json())
+            //Se recupera las entidades de noticias
+            .then(data => setNews(data.newsEntities))
+    }, []);
 
 function News() {
     return (
