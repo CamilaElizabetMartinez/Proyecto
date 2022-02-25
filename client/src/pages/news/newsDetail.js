fa613647a7d06a77b3173593844d6753c489731a
camila martinez <martinezce_@hotmail.com>
2022-02-25 19:36:22 +0200

    feat(client/src/pages/news): Creacion de detalle de noticia e imports

---
+++ client/src/pages/news/newsDetail.js
@@ -0,0 +1,11 @@
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { INDEX_NEWS_PUBLIC } from '../../settings'
+import Header3 from '../../components/Header3/Header3'
+import Carousel from '../../components/Carousel/Carousel'
+import Paragraph from '../../components/Paragraph/Paragraph'
+import Span from '../../components/Span/Span'
+
+const NewsDetail = () => {
+
+export default NewsDetail;
