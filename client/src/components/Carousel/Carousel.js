519c20f7d56fa2c83663ee12cbb314e53e9fc3cc
camila martinez <martinezce_@hotmail.com>
2023-02-06 17:33:43 +0200

    refactor(components/Carousel): Creacion del componente carousel  y declarado prop types

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,45 +1,28 @@
-import Carousel from 'react-bootstrap/Carousel';
-import "./carousel.css";
+import Carousel from 'react-material-ui-carousel'
+import { Paper } from '@mui/material'
+import PropTypes from 'prop-types'
+import Image from '../Image/Image'
 
-export default function carousel() {
-    return (
-        <Carousel>
-            <Carousel.Item interval={10000}>
-                <img
-                    className="d-block w-150"
-                    src={"/Images/carousel/canicas.jpeg"}
-                    alt="First slide"
-                    width="100%"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={10000}>
-                <img
-                    className="d-block w-250"
-                    src={"/Images/carousel/canicas.jpeg"}
-                    alt="Second slide"
-                    width="100%"
-                />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-300"
-                    src={"/Images/carousel/canicas.jpeg"}
-                    alt="Third slide"
-                    width="100%"
-                />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
-};
+const Carousell = ({images}) => {
+    return(
+        <Carousel
+        next={(next, active) => console.log(`we left ${active}, and are now at ${next}`)}
+        prev={(prev, active) => console.log(`we left ${active}, and are now at ${prev}`)}>
+        {
+            images && images.map((theImage, i) => <Item key={i} theImageUrl={theImage} />)
+        }
+    </Carousel>
+    )
+    function Item({ theImageUrl }) {
+        return (
+            <Paper>
+                <Image src={theImageUrl} />
+            </Paper>
+        )
+    }
+}
+
+Carousel.propTypes = {
+   Images: PropTypes.array.isRequired
+}
+export default Carousell; 
